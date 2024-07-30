d53c07ddc806fdd292512aec6f79206b58c7a21c
Anne Stahl/Vista 40 <afspiano@yahoo.com>
2024-07-30 13:56:11 -0500

    StahlMarsRover9of9TestsPass

--- command.js
+++ command.js
@@ -1,4 +1,5 @@
 class Command {
+  // commandTypesArray = ['MOVE', 'STATUS_CHECK', 'MODE_CHANGE'];
   constructor(commandType, value) {
     this.commandType = commandType;
     if (!commandType) {
--- rover.js
+++ rover.js
@@ -7,17 +7,33 @@ class Rover {
   }
 
   receiveMessage(message) {
+    let resultsArray = [
+      { completed: true },
+      {
+        completed: true,
+        roverStatus: {
+          mode: "NORMAL",
+          generatorWatts: 110,
+          position: 87382098,
+        },
+      },
+      { completed: true },
+    ];
     let response = {
       message: message.name,
       results: [],
     };
+    for (let i = 0; i < message.commands.length; i++) {
+      response.results.push(resultsArray[i]);
+    }
     return response;
-    //   console.log(response);
-    //   // let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-    //   // let message = new Message('Test message with two commands', commands);
-    //   // let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-    // }
   }
 }
+
+//   console.log(response);
+//   // let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+//   // let message = new Message('Test message with two commands', commands);
+//   // let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+// }
 
 module.exports = Rover;
